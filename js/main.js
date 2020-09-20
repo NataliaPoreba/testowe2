fe31a43fd67f87a51b534347a28e2a8dad576c2c
Natalia Poreba <poreba.natalia.poreba@gmail.com>
2020-09-20 13:13:58 +0200

    commit 13:14

--- js/main.js
+++ js/main.js
@@ -1,55 +1,60 @@
 const idBtn = document.getElementById('id-btn');
 let order = {
-    pizza:'',
+    pizza: '',
     sosy: [],
-    name:'',
+    name: '',
     adress: '',
-    email:'',
-    tel:'',
+    email: '',
+    tel: '',
     text: '',
-
+    napoje: []
 }
-
-
 idBtn.addEventListener("click", takeOrder)
-
-function takeOrder() {
-    sprawdzanieSosow("sosy")
-    const pomidorowy = document.getElementById('pomidorowy')
-    const czosnkowy = document.getElementById('czosnkowy')
-    const ostryPomidorowy = document.getElementById('ostryPomidorowy')
-    console.log(pomidorowy.checked)
-
-    //pobieram imie i nazwisko z input
-const nameC = document.getElementById('nameC').value
-order.name = nameC
-console.log("takeOrder -> nameC", order)
-}
-
-function sprawdzanieSosow(nazwaKlasy) {
+function sprawdzaniecheckBox(nazwaKlasy) {
+    console.log(nazwaKlasy)
     const pobraneWszytkieSosyZinputu = document.getElementsByClassName(nazwaKlasy)
-    for (let i = 0; i < pobraneWszytkieSosyZinputu.length; i++) {
-        // console.log(pobraneWszytkieSosyZinputu[i])
-        if (pobraneWszytkieSosyZinputu[i].checked === true) {
-            console.log(pobraneWszytkieSosyZinputu[i].attributes)
+    if (nazwaKlasy === "sosy") {
+        for (let i = 0; i < pobraneWszytkieSosyZinputu.length; i++) {
+            order.sosy.push(pobraneWszytkieSosyZinputu[i].checked)
+        }
+    } else {
+        for (let i = 0; i < pobraneWszytkieSosyZinputu.length; i++) {
+            order.napoje.push(pobraneWszytkieSosyZinputu[i].checked)
         }
     }
 }
-
-
-
-order.name
-idBtn.addEventListener("click", takeOrder)
-
-
-
-
+function takeOrder() {
+    sprawdzaniecheckBox("sosy")
+    sprawdzaniecheckBox("napoje")
+    // pobieram imię i nazwisko z input 
+    const nameC = document.getElementById('nameC').value
+    order.name = nameC
+    // pobieram email z input 
+    const email = document.getElementById('email').value
+    order.email = email
+    // pobieram telefon z input 
+    const tel = document.getElementById('tel').value
+    order.tel = tel
+    // pobieram adress
+    const adress = document.getElementById('adress')
+    order.adress = adress
+    // pobieram textarea 
+    const msg = document.getElementById('msg').value
+    order.text = msg
+    console.log("takeOrder -> nameC", order)
+}
+// function odejmowanie() {
+//     console.log(1 == "1")
+//     console.log(2 + "1")
+//     let j = 152558
+//     console.log(typeof j)
+// }
+// odejmowanie()
 // function dodawanie(a, b) {
 //     let wynik = a + b
 //     console.log(wynik)
 // }
 // dodawanie(5, 15)
-
 // dodawanie(2, 15)
 // dodawanie(7, 1885)
 // dodawanie(5, 88)
