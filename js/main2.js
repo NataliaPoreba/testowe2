a52810550f43057bf9e2ed9eb3cb9d5f7dabfc7d
Natalia Poreba <poreba.natalia.poreba@gmail.com>
2020-09-21 12:15:02 +0200

    finish work - add js

---
+++ js/main2.js
@@ -0,0 +1,13 @@
+const a = [1,2,3,4]
+
+let b = a.filtr(item =>) {
+    return item >2
+})
+console.log(a)
+console.log(b)
+// for (let i=0 i <a.length; i++) {
+//     console.log (a[i])
+// }
+// a.forEach(item =>{
+//     console.log(item)
+// })
